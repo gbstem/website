c3e6b2574827e624d5b719c3dc873ca26225f08d
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-26 23:09:25 -0500

    add more customizable possibilities to slideshow component

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "react-bootstrap";
 
-function Slideshow({ slides, banner }) {
+function Slideshow({ slides, captions, imageStyling, captionStyling, banner}) {
   const [currentSlide, setCurrentSlide] = useState(0);
+  var captionSlides = captions;
+
+  if(captions === undefined || captions === null) {
+    captionSlides = new Array(slides.length);
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,7 +26,7 @@ function Slideshow({ slides, banner }) {
       height: 0,
       paddingBottom: "30%",
       overflow: 'hidden'
-    } : {}}>
+    } : {width:'fit-content', margin: 'auto'}}>
       <Carousel interval={3000} activeIndex={currentSlide} controls={false}>
         {slides.map((slide, index) => (
           <Carousel.Item key={index}>
@@ -31,8 +36,9 @@ function Slideshow({ slides, banner }) {
               <img
                 src={slide}
                 alt={`Slide ${index + 1}`}
-                className="img-fluid banner"
+                className="img-fluid banner" style = {imageStyling}
               />
+              <div style = {captionStyling}>{captionSlides[currentSlide]}</div>
             </div>
           </Carousel.Item>
         ))}
