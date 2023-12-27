ce0117c7a98328c4af3b3fd7a59bcca185ad79ca
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-27 04:30:58 -0500

    move colleges page

---
+++ src/pages/HomeComponents/Colleges.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../../styles.css';
+
+function Colleges() {
+  const colleges = [
+    { name: 'Harvard', logo: 'https://1000logos.net/wp-content/uploads/2017/02/Harvard-Logo.png' },
+    { name: 'MIT', logo: 'https://download.logo.wine/logo/Massachusetts_Institute_of_Technology/Massachusetts_Institute_of_Technology-Logo.wine.png' },
+    { name: 'University of Chicago', logo: 'https://i0.wp.com/biocars.uchicago.edu/wp-content/uploads/2019/05/cropped-logo.png?ssl=1' },
+    { name: 'Brown University', logo: 'https://smartwatermagazine.com/sites/default/files/brown_university_logo.jpg' },
+    { name: 'UMass Amherst', logo: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/UMass_Seal_Medium_PMS_202.png' },
+    { name: 'Worcester Polytechnic Institute', logo: 'https://buildagrad.com/content/v2/images/wpi/WPI_Seal_FulClr.jpg' },
+    { name: 'NYU', logo: 'https://yt3.ggpht.com/-RZYi5isxH_M/AAAAAAAAAAI/AAAAAAAAAAA/rmWpoe2qZzI/s900-c-k-no/photo.jpg' },
+    { name: 'Georgetown', logo: 'https://www.georgetown.edu/wp-content/uploads/2019/10/Georgetown_logo_blueRGB.jpg' },
+    { name: 'Boston University', logo: 'https://content.sportslogos.net/logos/30/619/full/boston_university_terriers_logo_secondary_2015_sportslogosnet-5796.png' },
+    { name: 'UCLA', logo: 'https://1000logos.net/wp-content/uploads/2017/11/UCLA-Logo.png' },
+    { name: 'Columbia', logo: 'https://admissions.ucr.edu/sites/default/files/styles/form_preview/public/2020-07/ucr-education-logo-columbia-university.png?itok=-0FD6Ma2' },
+  ];
+
+
+  return (
+    <div className="container pb-5 pt-5">
+      <h1 className="text-center">Instructor Matriculation</h1>
+      <div className="scrolling-wrapper">
+        {colleges.map((college, index) => (
+          <div key={index} className="college-logo">
+            <img src={college.logo} alt="College Logo" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Colleges;
