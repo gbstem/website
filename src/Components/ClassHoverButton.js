b9c96f051b5718601e873137e4ecac979020a42e
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-12-27 19:28:35 -0500

    fix class button sizing on mobile

--- src/Components/ClassHoverButton.js
+++ src/Components/ClassHoverButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles.css';
 
 const ClassHoverButton = (props) => {
     return (
-        <Link to={props.link} style={{ fontWeight: "500", fontSize: "1.25rem", color:`${props.color}` }} className="link"><div style={{ borderWidth: "5px", borderStyle: "solid", borderRadius: "25px", padding: "2.25rem 0.5rem", width: "15rem", textAlign: "center", margin: "auto"}} className={`${props.fillButtonColor} link`}>{props.className}</div></Link>
+        <Link to={props.link} style={{ fontWeight: "500", fontSize: "1.25rem", color:`${props.color}` }} className="link"><div style={{ borderWidth: "5px", borderStyle: "solid", borderRadius: "25px", padding: "2.25rem 0.5rem", textAlign: "center", margin: "auto"}} className={`${props.fillButtonColor} link classButton`}>{props.className}</div></Link>
     )
 }
 
