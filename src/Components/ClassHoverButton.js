3f5c351dc5fca6e4be63d357a58a2476feb00967
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-07-21 22:39:35 -0400

    update button styling

--- src/Components/ClassHoverButton.js
+++ src/Components/ClassHoverButton.js
@@ -4,7 +4,7 @@ import '../styles.css';
 
 const ClassHoverButton = (props) => {
     return (
-        <Link to={props.link} style={{ fontWeight: "500", fontSize: "1.25rem", color:`${props.color}` }} className="link"><div style={{ borderWidth: "5px", borderStyle: "solid", borderRadius: "25px", padding: "2.25rem 0.5rem", textAlign: "center", margin: "auto"}} className={`${props.fillButtonColor} link classButton`}>{props.className}</div></Link>
+        <Link to={props.link} style={{ fontWeight: "500", fontSize: "1.25rem", color: 'white'}} className="link"><div style={{ backgroundColor:`${props.color}`, borderWidth: "5px", borderStyle: "solid", borderColor:`${props.color}`, borderRadius: "25px", padding: "2.25rem 0.5rem", textAlign: "center", margin: "auto"}} className={`${props.fillButtonColor} link classButton`}>{props.className}</div></Link>
     )
 }
 
