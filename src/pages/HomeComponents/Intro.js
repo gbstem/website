34ae9b1e3d2afb8c3be5d07c9198c21280dc6c8b
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-01-26 13:57:07 -0500

    update for spring

--- src/pages/HomeComponents/Intro.js
+++ src/pages/HomeComponents/Intro.js
@@ -7,7 +7,7 @@ function HomeIntro() {
       <p style = {{fontSize:"1.25rem"}}>
         The Greater Boston STEM Program (gbSTEM) delivers free introductory computer science, math,
         engineering, and science enrichment to elementary and middle school students.</p> 
-        <p>The fall semester will run from Sunday, September 29th to Saturday, December 21st. <br/> Instructor applications and student registrations closed on Friday, September 20th. If you are interested in gbSTEM's programs or hope to apply next semester as an instructor, please join our mailing list <a href="https://docs.google.com/forms/d/e/1FAIpQLSdKlEDGGKCc7bB7Xvlm3zOnhgh37MusQ_uU_6paHAhpHIDgXw/viewform?usp=sf_link">here.</a>
+        <p>The spring semester will run from Sunday, March 16th to Saturday, June 14th. <br/> Instructor applications and student signups for the spring semester will open on Sunday, February 9th. <br/><br/> Class enrollment is on a first-come, first-served basis and will open on Saturday, March 8th. You must first sign up through the portal before March 8th in order to enroll in classes when enrollment opens. 
         {/* To be notified when registrations and applications open, fill out this <a
           href="https://forms.gle/ejSvEu2cwwdovUg18"
           target="_blank" rel="noopener noreferrer"
@@ -24,7 +24,7 @@ function HomeIntro() {
         </div>
         <div className="col-md-6 p-3 d-flex justify-content-center align-items-center flex-column">
           <p>
-            <strong>High School and College Students </strong>
+            <strong>Instructor Applicants (High School or Older)</strong>
           </p>
           <a className="btn btn-primary" href="https://docs.google.com/forms/d/e/1FAIpQLSdKlEDGGKCc7bB7Xvlm3zOnhgh37MusQ_uU_6paHAhpHIDgXw/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
             Apply to teach
