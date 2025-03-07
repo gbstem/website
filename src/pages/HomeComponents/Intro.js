958c38b43d952641e509c70b5841034a56df0616
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-03-07 01:25:25 -0500

    date

--- src/pages/HomeComponents/Intro.js
+++ src/pages/HomeComponents/Intro.js
@@ -7,7 +7,7 @@ function HomeIntro() {
       <p style = {{fontSize:"1.25rem"}}>
         The Greater Boston STEM Program (gbSTEM) delivers free introductory computer science, math,
         engineering, and science enrichment to elementary and middle school students.</p> 
-        <p>The spring semester will run from Sunday, March 16th to Saturday, June 14th. <br/> Instructor applications and student signups for the spring semester will open on Sunday, February 9th. <br/><br/> Class enrollment is on a first-come, first-served basis and will open on Saturday, March 8th. You must first sign up through the portal before March 8th in order to enroll in classes when enrollment opens. 
+        <p>The spring semester will run from Sunday, March 16th to Saturday, June 14th. <br/> Instructor applications and student signups for the spring semester opened on Sunday, February 9th. <br/><br/> Class enrollment is on a first-come, first-served basis and will open on Saturday, March 8th. You must first sign up through the portal before March 8th in order to enroll in classes when enrollment opens. 
         {/* To be notified when registrations and applications open, fill out this <a
           href="https://forms.gle/ejSvEu2cwwdovUg18"
           target="_blank" rel="noopener noreferrer"
