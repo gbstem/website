9872c070fb0e44d8068dc82f98a2a79547a61f06
Meeeeesh <66706680+Meeeeesh@users.noreply.github.com>
2025-03-30 23:25:47 -0500

    Update Intro.js

--- src/pages/HomeComponents/Intro.js
+++ src/pages/HomeComponents/Intro.js
@@ -7,7 +7,7 @@ function HomeIntro() {
       <p style = {{fontSize:"1.25rem"}}>
         The Greater Boston STEM Program (gbSTEM) delivers free introductory computer science, math,
         engineering, and science enrichment to elementary and middle school students.</p> 
-        <p>The fall semester will run from Sunday, September 29th to Saturday, December 21st. <br/> Instructor applications and student registrations closed on Friday, September 20th. If you are interested in gbSTEM's programs or hope to apply next semester as an instructor, please join our mailing list <a href="https://docs.google.com/forms/d/e/1FAIpQLSdKlEDGGKCc7bB7Xvlm3zOnhgh37MusQ_uU_6paHAhpHIDgXw/viewform?usp=sf_link">here.</a>
+        <p>The Spring Semester registration is now closed. Registrations for the Fall semester will open in August. <br/> If you are interested in gbSTEM's programs or hope to apply next semester as an instructor, please join our mailing list <a href="https://docs.google.com/forms/d/e/1FAIpQLSdKlEDGGKCc7bB7Xvlm3zOnhgh37MusQ_uU_6paHAhpHIDgXw/viewform?usp=sf_link">here.</a>
         {/* To be notified when registrations and applications open, fill out this <a
           href="https://forms.gle/ejSvEu2cwwdovUg18"
           target="_blank" rel="noopener noreferrer"
